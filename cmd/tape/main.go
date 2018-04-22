e2eb123aee00757fd8ee60796b54a896f0b587fe
nico <brun.nicolas21@gmail.com>
2018-04-22 17:02:10 +0200

    preserve option for create

--- cmd/tape/main.go
+++ cmd/tape/main.go
@@ -24,7 +24,7 @@ import (
 var commands = []*cli.Command{
 	{
 		Run:   runCreate,
-		Usage: "create [-c] <archive> <file,...>",
+		Usage: "create [-c] [-p] <archive> <file,...>",
 		Alias: []string{"make"},
 		Short: "create a new cpio or ar archives",
 		Desc:  "",
@@ -80,6 +80,8 @@ func usage() {
 }
 
 func runCreate(cmd *cli.Command, args []string) error {
+	// recurse := cmd.Flag.Bool("r", false, "recurse")
+	preserve := cmd.Flag.Bool("p", false, "preserve")
 	compress := cmd.Flag.Bool("c", false, "compress")
 	if err := cmd.Flag.Parse(args); err != nil {
 		return err
@@ -100,9 +102,9 @@ func runCreate(cmd *cli.Command, args []string) error {
 	var err error
 	switch e := filepath.Ext(cmd.Flag.Arg(0)); e {
 	case ".cpio":
-		err = createCPIO(cmd.Flag.Arg(0), files, *compress)
+		err = createCPIO(cmd.Flag.Arg(0), files, *compress, *preserve)
 	case ".ar":
-		err = createAR(cmd.Flag.Arg(0), files, *compress)
+		err = createAR(cmd.Flag.Arg(0), files, *compress, *preserve)
 	default:
 		return fmt.Errorf("tape: can not create %s archive", e)
 	}
@@ -158,7 +160,7 @@ func runList(cmd *cli.Command, args []string) error {
 	return err
 }
 
-func createAR(a string, files []string, compress bool) error {
+func createAR(a string, files []string, compress, preserve bool) error {
 	f, err := os.Create(a)
 	if err != nil {
 		return err
@@ -200,8 +202,11 @@ func createAR(a string, files []string, compress bool) error {
 			Mode:     int64(i.Mode()),
 			Length:   int64(buf.Len()),
 		}
-		if i, ok := i.Sys().(*syscall.Stat_t); ok {
+		if i, ok := i.Sys().(*syscall.Stat_t); ok && preserve {
 			h.Uid, h.Gid = int64(i.Uid), int64(i.Gid)
+		} else {
+			h.Uid, h.Gid = int64(os.Geteuid()), int64(os.Getgid())
+			h.ModTime = time.Now()
 		}
 		if err := w.WriteHeader(&h); err != nil {
 			return err
@@ -213,7 +218,7 @@ func createAR(a string, files []string, compress bool) error {
 	return nil
 }
 
-func createCPIO(a string, files []string, compress bool) error {
+func createCPIO(a string, files []string, compress, preserve bool) error {
 	f, err := os.Create(a)
 	if err != nil {
 		return err
@@ -259,6 +264,10 @@ func createCPIO(a string, files []string, compress bool) error {
 			h.Minor = int64(i.Dev & 0xFFFFFFFF)
 			h.Links = int64(i.Nlink)
 			h.Inode = int64(i.Ino)
+		}
+		if !preserve {
+			h.Uid, h.Gid = int64(os.Geteuid()), int64(os.Getgid())
+			h.ModTime = time.Now()
 		}
 		if err := w.WriteHeader(&h); err != nil {
 			return err
