0447b9920a811469d81b3811e041686a4772fed2
nico <brun.nicolas21@gmail.com>
2018-04-22 14:36:18 +0200

    list cpio archive

--- cmd/tape/main.go
+++ cmd/tape/main.go
@@ -153,6 +153,17 @@ func listAR(file, block string, verbose bool) error {
 
 		logger.SetOutput(w)
 	}
+	var coeff int64
+	switch block {
+	default:
+		coeff = 1
+	case "K", "k":
+		coeff = 1024
+	case "M", "m":
+		coeff = 1024 * 1024
+	case "G", "g":
+		coeff = 1024 * 1024 * 1024
+	}
 	for {
 		h, err := a.Next()
 		switch err {
@@ -165,6 +176,7 @@ func listAR(file, block string, verbose bool) error {
 		if !verbose {
 			logger.Println(h.Filename)
 		} else {
+			h.Length /= coeff
 			printHeaderAR(logger, h)
 		}
 		if _, err := io.CopyN(ioutil.Discard, a, h.Length); err != nil {
@@ -181,12 +193,62 @@ func listCPIO(file, block string, verbose bool) error {
 	}
 	defer f.Close()
 	c := cpio.NewReader(f)
-	_ = c
+
+	logger := log.New(os.Stdout, "", 0)
+	logger.Println(file + ":")
+	if verbose {
+		w := tabwriter.NewWriter(os.Stdout, 6, 2, 2, ' ', 0)
+		defer w.Flush()
+
+		logger.SetOutput(w)
+	}
+	var coeff int64
+	switch block {
+	default:
+		coeff = 1
+	case "K", "k":
+		coeff = 1024
+	case "M", "m":
+		coeff = 1024 * 1024
+	case "G", "g":
+		coeff = 1024 * 1024 * 1024
+	}
+	for {
+		h, err := c.Next()
+		switch err {
+		case nil:
+		case io.EOF:
+			return nil
+		default:
+			return err
+		}
+		if !verbose {
+			logger.Println(h.Filename)
+		} else {
+			h.Length /= coeff
+			printHeaderCPIO(logger, h)
+		}
+		if _, err := io.CopyN(ioutil.Discard, c, h.Length); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func extractAR() error   { return nil }
 func extractCPIO() error { return nil }
+
+func printHeaderCPIO(w *log.Logger, h *cpio.Header) {
+	n := time.Now()
+	var f string
+	if n.Year() == h.ModTime.Year() {
+		f = "Jan 02 15:05"
+	} else {
+		f = "Jan 02  2006"
+	}
+	m := strings.Join(parseMode(h.Mode), "")
+	w.Printf("%s\t%s\t%s\t%d\t%s\t%s", m, h.User(), h.Group(), h.Length, h.ModTime.Format(f), h.Filename)
+}
 
 func printHeaderAR(w *log.Logger, h *ar.Header) {
 	n := time.Now()
